412f594620dcfb0ea51ec2ea359e4cfeddfae732
ALTHEA KATHLEEN B CARPENTERO <145583238+Yuikkaa@users.noreply.github.com>
2024-08-15 18:35:54 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -1,18 +1,11 @@
-public class TableAndChairs {
+public class TableandChairs
+{
     public static void main(String[] args) {
-     
-        for (int i = 0; i < 2; i++) {
-            System.out.println("X                                X");
-        }
-
+        System.out.println("X                                X");
+        System.out.println("X                                X");
         System.out.println("X       XXXXXXXXXXXXXXXXXX       X");
-
-        for (int i = 0; i < 3; i++) {
-            if (i == 0) {
-                System.out.println("XXXXXX  X                X   XXXXX");
-            } else {
-                System.out.println("X    X  X                X   X   X");
-            }
-        }
+        System.out.println("XXXXXX  X                X   XXXXX");
+        System.out.println("X    X  X                X   X   X");
+        System.out.println("X    X  X                X   X   X");
     }
 }
