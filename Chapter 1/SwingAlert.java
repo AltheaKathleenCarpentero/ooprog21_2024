87cbf6b14cbb73f8c62a773ab1aa598c84aff014
ALTHEA KATHLEEN B CARPENTERO <145583238+AltheaKathleenCarpentero@users.noreply.github.com>
2024-08-21 19:47:42 +0800

    Add files via upload

---
+++ Chapter 1/SwingAlert.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class SimpleMessage {
+    public static void main(String[] args) {
+        
+        JOptionPane.showMessageDialog(null, "Hello World!");
+    }
+}
