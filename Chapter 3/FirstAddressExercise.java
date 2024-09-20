24feab8e38519f1ccbcf8d871efb37eb55778349
ALTHEA KATHLEEN B CARPENTERO <145583238+AltheaKathleenCarpentero@users.noreply.github.com>
2024-09-20 21:55:48 +0700

    Add files via upload

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,12 @@
+public class FirstAddressExercise {
+    public static void main(String[] args) {
+        System.out.println("First Java Application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("VERTU Company");
+        System.out.println("RM 505, 5F, Beverley Commercial Centre, 87-105 Chatham Road South, \nTsim Sha Tsui, Kowloon, Hongkong");
+        System.out.println("Company: Vertu International Co., Ltd.");
+    }
+}
