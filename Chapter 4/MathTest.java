ec280f9b89cf6693c802a3e11d0c5a072041805c
ALTHEA KATHLEEN B CARPENTERO <145583238+AltheaKathleenCarpentero@users.noreply.github.com>
2024-10-02 19:59:01 +0700

    Add files via upload

---
+++ Chapter 4/MathTest.java
@@ -0,0 +1,22 @@
+public class MathTest {
+
+    public static void main(String[] args) {
+  
+        System.out.println("SQRT: " + Math.sqrt(123));
+
+        System.out.println("SIN: " + Math.sin(Math.toRadians(210))); 
+ 
+        System.out.println("COS: " + Math.cos(Math.toRadians(150))); 
+        System.out.println("FLOOR: " + Math.floor(28.7));
+
+        System.out.println("CEIL: " + Math.ceil(22.7));
+
+        System.out.println("ROUND: " + Math.round(22.7));
+
+        System.out.println("MAX: " + Math.max(68, 71));
+
+        System.out.println("MIN: " + Math.min(68, 71));
+
+        System.out.println("RANDOM: " + Math.random());
+    }
+}
