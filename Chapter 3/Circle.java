ac2195fe8cc3ccfa704b864fd746a82249291526
ALTHEA KATHLEEN B CARPENTERO <145583238+AltheaKathleenCarpentero@users.noreply.github.com>
2024-10-04 08:44:03 +0700

    Add files via upload

---
+++ Chapter 3/Circle.java
@@ -0,0 +1,30 @@
+public class Circle {
+
+    private double radius;
+    private double diameter;
+    private double area;
+
+    public Circle() {
+        radius = 1;
+        diameter = 2 * radius;
+        area = Math.PI * Math.pow(radius, 2);
+    }
+
+    public void setRadius(double rad) {
+        radius = rad;
+        diameter = 2 * radius;
+        area = Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double getDiameter() {
+        return diameter;
+    }
+
+    public double getArea() {
+        return area;
+    }
+} 
